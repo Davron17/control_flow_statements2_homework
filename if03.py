7603b717568b90698fbc0688703aae1e61c0bcc2
Davron17 <dorazaliyev75@gmail.com>
2023-10-05 00:18:15 +0500

    3-misol

--- if03.py
+++ if03.py
@@ -8,4 +8,16 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    max=a
+    small=a
+    if max<b:
+        max=b
+    if max<c:
+        max=c
+    if small>b:
+        small=b
+    if small>c:
+        small=c
+    return a+b+c-small-max
+print(main(5,6,3))
+print(main(4,9,78))
