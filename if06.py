acc7ea4e79c00bc2f6cda49816f34e9a103f9b45
Davron17 <dorazaliyev75@gmail.com>
2023-10-05 00:36:01 +0500

    6-misol

--- if06.py
+++ if06.py
@@ -6,4 +6,21 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    largest=n%10
+    index=1
+    if n//10000>n%10:
+        largest=n//10000
+        index=5
+    if (n-n//10000*10000)//1000>n%10:
+        largest=(n-n//10000*10000)//1000
+        index=4
+    if ((n-n//1000*1000)//100)>n%10:
+        largest=(n-n//1000*1000)//100
+        index=3
+    if (n-n//100*100)//10>n%10:
+        largest=(n-n//100*100)//10
+        index=2
+    return largest,index
+print(main(50684))
+print(main(12345))
+    
