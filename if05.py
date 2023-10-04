347bc4a78c07f70239f6b569ccf2e5f7c40341be
Davron17 <dorazaliyev75@gmail.com>
2023-10-05 00:31:06 +0500

    5-misol

--- if05.py
+++ if05.py
@@ -6,4 +6,15 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    largest=n%10
+    if n//10000>n%10:
+        largest=n//10000
+    if (n-n//10000*10000)//1000>n%10:
+        largest=(n-n//10000*10000)//1000
+    if ((n-n//1000*1000)//100)>n%10:
+        largest=(n-n//1000*1000)//100
+    if (n-n//100*100)//10>n%10:
+        largest=(n-n//100*100)//10
+    return largest
+print(main(50684))
+print(main(12345))
